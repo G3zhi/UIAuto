2161c7bbc758934d28e1bd0bf7873c6c8ecb064c
G3zhi <guoxiongzhi@126.com>
2017-05-04 23:11:54 +0800

    1

--- SmartFlow/src/test/java/UIAuto/SmartFlow/PageObject/Login_Page.java
+++ SmartFlow/src/test/java/UIAuto/SmartFlow/PageObject/Login_Page.java
@@ -3,8 +3,15 @@ package UIAuto.SmartFlow.PageObject;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.fluentlenium.core.FluentPage;
+import org.fluentlenium.core.domain.FluentWebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class Login_Page extends FluentPage{
+	
+	@FindBy(id="login")
+	private FluentWebElement login_Input;
+	
+
 	
 	   public String getUrl(){
 	        return "http://54.223.147.111/fulltest1/login.jsp";
@@ -17,7 +24,8 @@ public class Login_Page extends FluentPage{
 	   }
 	    
 	    public void login_username(String username){
-	        $("#login").fill().with(username);
+	    	login_Input.fill().with(username);
+	        //$("#login").fill().with(username);
 	    }
 	    
 	    public void login_password(String password){
